a5197f2e87f5b64c024ea48e9fbf50152ce89aa2
Demian Turner <demian@muse23.com>
2020-06-11 13:50:19 +0100

    Initial Commit

---
+++ LeetCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LeetCode
+//
+//  Created by Demian Turner on 11/06/2020.
+//  Copyright © 2020 Demian Turner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by Demian Turner on 11/06/2020.
+//  Copyright © 2020 Demian Turner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
